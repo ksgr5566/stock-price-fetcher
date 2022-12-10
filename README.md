da30f9a60b0e8085a5fd11224760068c8af87e9f
ksgr5566 <ksgr5566@gmail.com>
2022-12-10 17:26:35 +0530

    Init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Usage example
+<br>
+
+```
+const spf = require("stock-price-fetcher")
+const data = await spf.data('AAPL'); // param should be a valid yahoo finance ticker
+console.log(data) // { currency: 'USD', price: 148.11 }
+```
---
+++ index.js
@@ -0,0 +1,35 @@
+const baseUrl = 'https://finance.yahoo.com/quote/';
+
+const data = async function (ticker) {
+    return new Promise(async (resolve, reject) => {
+            try {
+                const response = await fetch(`${baseUrl}${ticker}`, {
+                    method: 'GET'
+                });
+                // console.log(response);
+                const body = (await response.text())
+                let price = body.split(`"${ticker}":{"sourceInterval"`)[1]
+                    .split('regularMarketPrice')[1]
+                    .split('fmt":"')[1]
+                    .split('"')[0];
+
+                price = parseFloat(price.replace(',', ''));
+
+                const currencyMatch = body.match(/Currency in ([A-Za-z]{3})/);
+                let currency = null;
+                if (currencyMatch) {
+                    currency = currencyMatch[1];
+                }
+
+                resolve({
+                    currency,
+                    price,
+                });
+            } catch (err) {
+                reject(err);
+            }
+        });
+};
+
+// data("AAPL").then(res => console.log(res)).catch(err => console.log(err));
+exports.data = data
